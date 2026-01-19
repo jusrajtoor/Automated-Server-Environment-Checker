4ece957e7c9fc722ba3592a8e58b92828fb58819
jusrajtoor <142118079+jusrajtoor@users.noreply.github.com>
2026-01-19 00:50:16 -0500

    refactor

--- ADPasswordExpiryNotification.ps1
+++ ADPasswordExpiryNotification.ps1
@@ -1,39 +1,67 @@
 Import-Module C:\Scripts\SendEmail\MailModule.psm1
-$MailAccount=Import-Clixml -Path C:\Scripts\SendEmail\outlook.xml
-$MailPort=587
-$MailSMTPServer="smtp-mail.outlook.com"
-$MailFrom=$MailAccount.UserName
-$MailTo="testjackedprogrammer@outlook.com"
 
-$HowManyDaysBeforeNotify=14
+$MailAccount = Import-Clixml -Path C:\Scripts\SendEmail\outlook.xml
+$MailPort = 587
+$MailSMTPServer = "smtp-mail.outlook.com"
 
-$Users=Get-ADUser -Filter * -Properties * -Server Jacked.ca | Where-Object {($_.Enabled -eq $true) -and ($_.PasswordNeverExpires -eq $false) -and ($_.PasswordExpired -eq $false)}
+$MailFrom = $MailAccount.UserName
+$MailTo   = "jusraj@hotmail.ca"
 
-foreach($User in $Users){
-    $Name = "$($User.givenname) $($user.Surname)"
-    $Email =$user.EmailAddress
-    $PasswordSetOn=$user.PasswordLastSet
-    $PasswordPolicy=(Get-ADUserResultantPasswordPolicy -Identity $User.SamAccountName -Server jacked.ca)
-    if($PasswordPolicy){
-        $MaxPasswordAge=$PasswordPolicy.maxPasswordAge
-    }else{
-        $MaxPasswordAge=(Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+$HowManyDaysBeforeNotify = 14
+
+
+$User = Get-ADUser `
+    -Identity "jusraj" `
+    -Properties * `
+    -Server "example.local"   
+
+if ($User -and $User.Enabled -and !$User.PasswordNeverExpires -and !$User.PasswordExpired) {
+
+    $Name = "Jusraj"
+    $Email = "jusraj@hotmail.ca"
+
+    $PasswordSetOn = $User.PasswordLastSet
+
+    $PasswordPolicy = Get-ADUserResultantPasswordPolicy `
+        -Identity $User.SamAccountName `
+        -Server "example.local"
+
+    if ($PasswordPolicy) {
+        $MaxPasswordAge = $PasswordPolicy.MaxPasswordAge
+    } else {
+        $MaxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
     }
 
-    $ExpiryDate=$PasswordSetOn+$MaxPasswordAge
-    $Today=Get-Date
-    $DaysLeft=(New-TimeSpan -Start $Today -End $ExpiryDate).Days
+    $ExpiryDate = $PasswordSetOn + $MaxPasswordAge
+    $Today = Get-Date
+    $DaysLeft = (New-TimeSpan -Start $Today -End $ExpiryDate).Days
 
-    if($DaysLeft -le $HowManyDaysBeforeNotify){
-        if($DaysLeft -ge 1){
-            $CustomMessage="in $DaysLeft days"
-        }else{
-            $CustomMessage="today"
+    if ($DaysLeft -le $HowManyDaysBeforeNotify) {
+
+        if ($DaysLeft -ge 1) {
+            $CustomMessage = "in $DaysLeft days"
+        } else {
+            $CustomMessage = "today"
         }
 
         $Subject = "Your password expires $CustomMessage"
-        $Body="<p>Hi $Name,</p><p>Your password expires $CustomMessage</p><p>Thanks,</p><p>JackedProgrammer</p>"
-        Send-MailKitMessage -From $MailFrom -To $MailTo -SMTPServer $MailSMTPServer -Port $MailPort -Credential $MailAccount -Subject $Subject -Body $Body -BodyAsHtml
+
+        $Body = @"
+<p>Hi Jusraj,</p>
+<p>Your password expires <strong>$CustomMessage</strong>.</p>
+<p>Please update your password to avoid any interruption.</p>
+<p>Thanks,<br/>Jusraj</p>
+"@
+
+        Send-MailKitMessage `
+            -From $MailFrom `
+            -To $MailTo `
+            -SMTPServer $MailSMTPServer `
+            -Port $MailPort `
+            -Credential $MailAccount `
+            -Subject $Subject `
+            -Body $Body `
+            -BodyAsHtml
     }
-    
 }
+
